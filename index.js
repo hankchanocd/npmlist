a60460243bf91fe0bf56dce1556f0a17b0d67e12
Hank Chan <hankchanth@icloud.com>
2018-03-23 07:38:07 -0700

    Add listing functions
    - add npm ls -l
    - add npm ls -g
    - add npm ls --info
    - add what used to be npmlatest

--- index.js
+++ index.js
@@ -2,30 +2,71 @@
 
 const ls = require('ls');
 const columnify = require('columnify');
+const program = require('commander');
+const chalk = require('chalk');
+const execChildProcess = require('child_process').exec;
+var read = require('read-file');
 
+// local modules
+const {
+    sortByDate,
+    parseDate
+} = require('./lib/date');
+
+// a list of globally installed node modules
 const list = ls('/usr/local/bin/lib/node_modules/*');
 
-// Select only the latest 5 download packages
-const result = sortByDate(list).slice(0, 5).map((item) => {
-    return {'name': item.name, 'time': parseDate(item.stat.mtime)}});
-console.log(columnify(result));
+program
+    .version('1.0.0', '-v, --version')
+    .usage(`[option]`)
+    .description('Listing npm packages in cli made easy')
+    .option('-g, --global', 'the command for npm list --global')
+    .option('-l, --local', 'the command for npm list --local')
+    .option('-i, --info', 'the command for what used to be npmlist --long')
+    .option('-t, --time', 'the command for what used to be npmlatest, showing the five last globally installed npm packages')
+    .on('--help', function () {
+        console.log();
+        console.log('  ' + chalk.redBright('This program has not yet provided any subcommands but plan to release some in the future.'));
+        console.log();
+    })
+    .parse(process.argv);
 
-// Helper methods
-// Sort by date descendingly
-function sortByDate(arr) {
-    arr.sort(function(a, b){
-        var keyA = new Date(a.stat.mtime),
-        keyB = new Date(b.stat.mtime);
 
-        // Compare the 2 dates
-        if(keyA < keyB) return 1;
-        if(keyA > keyB) return -1;
-        return 0;}
-        );
-    return arr;
+// Listing of installed packages are executed through 'child_process'
+let cmd = 'npm list --depth=0 ';
+if (program.global) {
+    cmd += '--global';
+    exec(cmd);
+} else if (program.local) {
+    cmd += '--local';
+    exec(cmd);
+} else if (program.time) {
+    // Select only the latest 5 download packages
+    const result = sortByDate(list).slice(0, 5).map((item) => {
+        return {
+            'name': item.name,
+            'time': parseDate(item.stat.mtime)
+        };
+    });
+    console.log(columnify(result));
+} else if (program.info) {
+    // sync 
+    var buffer = read.sync('lib/npminfo.sh', 'utf8');
+    exec(buffer);
+} else {
+    // If nothing specified...
+    cmd += '--local';
+    exec(cmd);
 }
 
-function parseDate(str) {
-    let date = new Date(str);
-    return `${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+
+// Helper methods
+function exec(command) {
+    execChildProcess(command, function (error, stdout, stderr) {
+        if (error) console.log(chalk.red.bold.underline("exec error:") +
+            error);
+        if (stdout) console.log(chalk.white(stdout));
+        if (stderr) console.log(chalk.red("Error: ") +
+            stderr);
+    });
 }
