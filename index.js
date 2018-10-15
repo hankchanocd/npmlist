742d42e8f03b129cec49794370cd2bb154570434
Hank Chan <hankchanth@icloud.com>
2018-10-15 14:13:39 -0700

    feat(https.js, index.js): Add --all to fetchModule
    Refactor the https.js architecture to use chain operations
    
    BREAKING CHANGE: All methods from https.js are now using chain operations

--- index.js
+++ index.js
@@ -30,13 +30,14 @@ program
     .option('-g, --global', 'list global dependencies')
     .option('-i, --info', 'add information to dependencies list')
     .option('-t, --time', 'show the last five globally installed npm packages')
+    .option('-a, --all [name]', 'show all npm packages')
     .option('-s, --scripts', 'list scripts/tasks')
-    .option('-d, --docs <args>', 'pretty print of docs/information from the given package')
     .on('--help', function () {
         console.log();
         console.log('  Examples:');
         console.log('    ' + chalk.blueBright(`npmlist -i -l, ${chalk.white('shows a detailed list of local modules/dependencies')}`));
-        console.log('    ' + chalk.blueBright(`npmlist [args], ${chalk.white("shows a module's dependencies from npm registry API")}`));
+        console.log('    ' + chalk.blueBright(`npmlist [args], ${chalk.white("shows dependencies of a module from npm registry")}`));
+        console.log('    ' + chalk.blueBright(`npmlist [args] --all, ${chalk.white("shows all info about a module from npm registry")}`));
         console.log();
     })
     .parse(process.argv);
@@ -63,10 +64,15 @@ if (program.global) {
 } else if (program.info) {
     npmListInfo().local();
 
-} else if (program.docs || program.args.length > 0) { // If a package is specified
+} else if (program.args.length > 0) { // If a package is specified
     // both independent args and '--doc args' can be used to retrieve a module's dependencies info
     const module = program.docs ? program.docs : program.args;
-    fetchModule(module);
+
+    if (!program.all) {
+        fetchModule(module).simple();
+    } else {
+        fetchModule(module).all();
+    }
 
 } else if (program.scripts) {
     npmScripts();
--- src/https.js
+++ src/https.js
@@ -1,55 +1,108 @@
 /**
- * https.js fetches lists of module dependencies from npm registry
+ * https.js fetches lists of module info from npm registry
+ * https.js uses chain operations that are divided into 2 categories: simple() and all()
  *
  */
 'use strict';
 
 // Dependencies
 const chalk = require('chalk');
+const ui = require('cliui')();
+const columnify = require('columnify');
 const {
     fetch
 } = require('./lib/fetch');
 
+
 /*
- * fetchModule has two options for output: simple() and all()
+ * fetchModule has two options for output: simple() and all(). Chain operations are flexible for future expansion with backward compatibility
+ * p.s. Lesson learned: async/await and methods that use promise cannot return
  */
-module.exports.fetchModule = async function (module) {
-    let response;
-    try {
-        response = await fetch(`https://registry.npmjs.org/${module}`);
-        if (!response.status == 200) {
-            throw Error('NPM registry returns' + response.status);
-        }
-    } catch(err) {
-        console.err(err);
-    }
-
-    let result = {};
-    result.module = module;
-    result.dependencies = parseFetchedList(response);
-
-    printFetchedList(result);
+module.exports.fetchModule = function (module) {
+    return {
+        simple: async function () {
+                try {
+                    let data = JSON.parse(await fetch(`https://registry.npmjs.org/${module}`));
+                    let result = {
+                        module: module,
+                        dependencies: parse(data).simple().dependencies
+                    };
+                    print(result).simple();
+                } catch (err) {
+                    console.log(chalk.redBright(err));
+                }
+            },
+            all: async function () {
+                try {
+                    let data = JSON.parse(await fetch(`https://registry.npmjs.org/${module}`));
+                    let result = {
+                        module
+                    };
+                    Object.assign(result, parse(data).all()); // Merge all the result into the first object: result
+                    print(result).all();
+                } catch (err) {
+                    console.log(chalk.redBright(err));
+                }
+            }
+    };
 };
 
-function parseFetchedList(data) {
-    const versions = Object.keys(JSON.parse(data).versions);
-    const latestVersion = versions[versions.length - 1];
-    const result = JSON.parse(data).versions[latestVersion].dependencies;
+/*
+ * Parsing with 2 options: simple() offers lazy evaluation, all() evaluates all defined rules
+ */
+function parse(data) {
+    const versions = Object.keys(data.versions);
+    const dependencies = (() => {
+        const latestVersion = versions[versions.length - 1];
+        const result = data.versions[latestVersion].dependencies; // List only the dependencies from that latest release
+        return result;
+    })();
 
-    return result;
+    return {
+        simple() {
+            return {
+                dependencies
+            };
+        },
+        all() {
+            return {
+                versions,
+                dependencies
+            };
+        }
+    };
 }
 
-function printFetchedList(result) {
+
+function print(result) {
     let module = result.module ? result.module : '';
     let dep = result.dependencies ? result.dependencies : '';
+    let versions = result.versions ? result.versions : '';
 
-    if (!dep) {
-        return console.log(chalk.blueBright(`${module} has no dependencies`));
-    }
+    return {
+        simple() { // Simple mode
+            if (!dep) { // Returns early if has no dependencies
+                return console.log(chalk.blueBright(`${module} has no dependencies`));
+            }
 
-    console.log(chalk.blueBright(`${module}'s Dependencies:`));
-    Object.keys(dep).forEach(key => {
-        let value = dep[key] ? dep[key].replace(/[^0-9.,]/g, "") : '';
-        return console.log('├── ' + key + '@' + chalk.grey(value));
-    });
+            console.log(chalk.blueBright(`${module}'s Dependencies:`));
+            Object.keys(dep).forEach(key => {
+                let value = dep[key] ? dep[key].replace(/[^0-9.,]/g, "") : '';
+                return console.log('├── ' + key + '@' + chalk.grey(value));
+            });
+        },
+        all() { // All mode
+            console.log(chalk.blueBright(module));
+            ui.div({
+                text: columnify(dep),
+                width: 30,
+                padding: [0, 2, 0, 2]
+            }, {
+                text: columnify(versions),
+                width: 25,
+                padding: [0, 2, 0, 2]
+            });
+            console.log(ui.toString());
+        }
+    };
 }
