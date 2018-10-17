8e4f32c900e448f19cef48bcaa4b261845f833fd
Hank Chan <hankchanth@icloud.com>
2018-10-16 17:03:36 -0700

    fix(npmRecent.js): Accept any global path
    Read global path for node modules on any machine

--- src/npmRecent.js
+++ src/npmRecent.js
@@ -5,7 +5,9 @@
 'use strict';
 
 // Dependencies
+const execChildProcess = require('child_process').exec;
 const ls = require('ls');
+const path = require('path');
 const chalk = require('chalk');
 const columnify = require('columnify');
 const {
@@ -13,24 +15,40 @@ const {
 	parseDate
 } = require('./utils/dateUtil');
 
-// a list of global node modules
-const GLOBAL_MODULES_LIST = ls('/usr/local/bin/lib/node_modules/*');
-
 
 /*
  * npmRecent.js has only one exposed function
  */
 module.exports = function () {
 	try {
-		const result = sortByDate(GLOBAL_MODULES_LIST).slice(0, 10).map((item) => {
-			return {
-				'name': item.name ? item.name : '',
-				'time': item.stat.mtime ? parseDate(item.stat.mtime) : ''
-			};
+		execChildProcess('npm root -g', function (error, stdout, stderr) {
+			if (error) {
+				return console.log(chalk.red.bold.underline("exec error:") + error);
+			}
+			if (stdout) {
+				listGlobalModulesByTime(stdout);
+			}
+			if (stderr) {
+				return console.log(chalk.red("Error: ") + stderr);
+			}
 		});
-		console.log(columnify(result));
 
-	} catch(err) {
+	} catch (err) {
 		console.log(chalk.redBright(err));
 	}
 };
+
+function listGlobalModulesByTime(stdout) {
+	// Use path to parse the given path, and attach the following string so to avoid the `/node_modules/\n ` nasty bug
+	const GLOBAL_MODULES_ALL = path.parse(stdout).dir + '/node_modules/*';
+	const GLOBAL_MODULES_LIST = ls(GLOBAL_MODULES_ALL);
+
+	let result = sortByDate(GLOBAL_MODULES_LIST).slice(0, 10).map((item) => {
+		return {
+			'name': item.name ? chalk.blueBright(item.name) : '',
+			'time': item.stat.mtime ? parseDate(item.stat.mtime) : ''
+		};
+	});
+
+	console.log(columnify(result));
+}
