1bcf09db24bccd26987742736363b80a64a44296
Hank Chan <hankchanth@icloud.com>
2018-10-17 17:26:12 -0700

    feat(cli.js): Add `--nofuzzy` flag
    Provide an option to disable fuzzy mode

--- bin/cli.js
+++ bin/cli.js
@@ -23,20 +23,29 @@ program
 	.version('3.0.0', '-v, --version')
 	.usage(`[option] [name]`)
 	.description('A fuzzy CLI that lists everything listable from any npm package')
+
+	// Five main features:
 	.option('-l, --local', 'list local dependencies, which is also the default mode')
 	.option('-g, --global', 'list global modules')
 	.option('-d, --details', 'include details to each dependency, but disable the default interactive mode')
 	.option('-t, --time', 'show the latest 5 modules installed globally')
-	.option('-a, --all [name]', 'show all information about a module fetched from NPM registry')
 	.option('-s, --scripts', 'list/execute npm scripts')
-	.option('-i, --interactive', 'enable interactive/fuzzy mode, which is now default')
-	.option('-n, --no-fuzzy', 'disable the fuzzy mode and resort to stdout')
+
+	// Fetch info from NPM registry
+	.option('-a, --all [name]', 'show all information about a module fetched from NPM registry')
+
+	// Mode
+	.option('-f, --fuzzy', 'enable fuzzy mode, which is now default on most features')
+	.option('-n, --nofuzzy', 'disable the fuzzy mode and resort to stdout')
+	.option('-i, --interactive', 'enable interactive mode')
+
+	// Help
 	.on('--help', function () {
 		console.log();
 		console.log('  Examples:');
-		console.log('    ' + chalk.blueBright(`npmlist, ${chalk.white('shows a list of local dependencies')}`));
+		console.log('    ' + chalk.blueBright(`npmlist, ${chalk.white('shows a fuzzy list of local dependencies')}`));
 		console.log('    ' + chalk.blueBright(`npmlist -g -d, ${chalk.white('shows a detailed list of global modules')}`));
-		console.log('    ' + chalk.blueBright(`npmlist [module], ${chalk.white("shows dependencies of a module from npm registry")}`));
+		console.log('    ' + chalk.blueBright(`npmlist [module], ${chalk.white("shows a fuzzy list of a module's dependencies fetched from registry")}`));
 		console.log('    ' + chalk.blueBright(`npmlist [module] --all, ${chalk.white("shows all info about a module from npm registry")}`));
 		console.log();
 	})
@@ -45,13 +54,29 @@ program
 
 if (program.global) {
 	(function listGlobalPackages() {
-		!program.details ? npmList().global().fuzzy() : npmListDetails().global();
+		if (!program.details) { // if details flag not specified
+			if (!program.nofuzzy) { // if nofuzzy flag not specified
+				npmList().global().fuzzy();
+			} else {
+				npmList().global().default();
+			}
+		} else {
+			npmListDetails().global();
+		}
 	})();
 
 
 } else if (program.local) {
 	(function listLocalDependencies() {
-		!program.details ? npmList().local().fuzzy() : npmListDetails().local();
+		if (!program.details) { // if details flag not specified
+			if (!program.nofuzzy) { // if nofuzzy flag not specified
+				npmList().local().fuzzy();
+			} else {
+				npmList().local().default();
+			}
+		} else {
+			npmListDetails().local();
+		}
 	})();
 
 
@@ -80,7 +105,11 @@ if (program.global) {
 
 
 } else if (program.scripts) {
-	npmScripts().fuzzy();
+	if (!program.nofuzzy) { // If nofuzzy flag not specified
+		npmScripts().fuzzy();
+	} else {
+		npmScripts().default();
+	}
 
 
 } else { // default mode when nothing specified...
