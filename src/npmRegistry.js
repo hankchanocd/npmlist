246f19ebd380989e87013a7c5b52b64861084928
Hank Chan <hankchanth@icloud.com>
2019-01-02 04:12:16 -0800

    style(npmRegistry.js): Update

--- src/npmRegistry.js
+++ src/npmRegistry.js
@@ -3,17 +3,14 @@
  * It uses chain operations that are divided into 2 categories: simple() and all()
  *
  */
-'use strict';
+"use strict";
 
 // Dependencies
-const chalk = require('chalk');
-const ui = require('cliui')();
-const columnify = require('columnify');
-const {
-	fetch
-} = require('./utils/promiseUtil');
-const StringUtil = require('./utils/stringUtil');
-
+const chalk = require("chalk");
+const ui = require("cliui")();
+const columnify = require("columnify");
+const { fetch } = require("./utils/promiseUtil");
+const StringUtil = require("./utils/stringUtil");
 
 /*
  * npmRegistry has only one exposed export function.
@@ -21,15 +18,14 @@ const StringUtil = require('./utils/stringUtil');
  * Chain operations are flexible for future expansion with backward compatibility
  *
  */
-module.exports.main = async function (module = '') {
+module.exports.main = async function(module = "") {
 	if (!module) {
-		throw chalk.redBright('No module provided');
+		throw chalk.redBright("No module provided");
 	}
 
 	let data;
 	try {
 		data = JSON.parse(await fetch(`https://registry.npmjs.org/${module}`));
-
 	} catch (err) {
 		console.log(chalk.redBright(err));
 		process.exit(1); // Abort early if fetching fails
@@ -42,19 +38,19 @@ module.exports.main = async function (module = '') {
 
 			// Second-level chain operation has default(), raw(), rawNoColor()
 			return {
-				default () {
+				default() {
 					if (!list || list.length === 0) return;
 
 					return list.forEach(i => console.log(i));
 				},
 
-				raw: async function () {
+				raw: async function() {
 					if (!list || list.length === 0) return;
 
 					return list;
 				},
 
-				rawNoColor: async function () {
+				rawNoColor: async function() {
 					if (!list || list.length === 0) return;
 
 					return list.map(key => {
@@ -64,7 +60,6 @@ module.exports.main = async function (module = '') {
 					});
 				}
 			};
-
 		},
 		all() {
 			let list = parseToList(data).all();
@@ -73,28 +68,28 @@ module.exports.main = async function (module = '') {
 	};
 };
 
-
 /*
  * Parsing with 2 options: simple() offers lazy evaluation, all() evaluates all defined rules
  */
-function parseToList(data = {
-	'name': '',
-	'dist-tags': {
-		'latest': ''
-	},
-	'versions': {}
-}) {
-	if (!data['name'] || !data['dist-tags'] || !data['versions']) {
-		throw new Error('Fetched info is incomplete, therefore useless');
+function parseToList(
+	data = {
+		name: "",
+		"dist-tags": {
+			latest: ""
+		},
+		versions: {}
+	}
+) {
+	if (!data["name"] || !data["dist-tags"] || !data["versions"]) {
+		throw new Error("Fetched info is incomplete, therefore useless");
 	}
 
-	const title = data['name'] + '@' + data['dist-tags']['latest'];
+	const title = data["name"] + "@" + data["dist-tags"]["latest"];
 	const versions = Object.keys(data.versions);
 	const dependencies = (() => {
 		let latestVersion = versions[versions.length - 1];
 		return data.versions[latestVersion].dependencies; // List only the dependencies from that latest release
 	})();
-
 
 	// Chain operation has simple() and all()
 	return {
@@ -106,23 +101,28 @@ function parseToList(data = {
 			console.log(chalk.blueBright(`${title}'s Dependencies:`));
 
 			return Object.keys(dependencies).map(key => {
-				let value = dependencies[key] ? dependencies[key].replace(/[^0-9.,]/g, "") : '';
-				return '├── ' + key + '@' + chalk.grey(value);
+				let value = dependencies[key]
+					? dependencies[key].replace(/[^0-9.,]/g, "")
+					: "";
+				return "├── " + key + "@" + chalk.grey(value);
 			});
 		},
 		all() {
 			console.log(chalk.blueBright(title));
-			ui.div({
-				text: columnify(dependencies),
-				width: 30,
-				padding: [0, 2, 0, 2]
-			}, {
-				text: columnify(versions),
-				width: 25,
-				padding: [0, 2, 0, 2]
-			});
+			ui.div(
+				{
+					text: columnify(dependencies),
+					width: 30,
+					padding: [0, 2, 0, 2]
+				},
+				{
+					text: columnify(versions),
+					width: 25,
+					padding: [0, 2, 0, 2]
+				}
+			);
 
-			return ui.toString().split('\n');
+			return ui.toString().split("\n");
 		}
 	};
 }
